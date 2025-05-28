d6c89fe9a06bfb0434ed0467e9bed6f4ee106288
SohamDesai1 <sohamdesai123@gmail.com>
2025-05-28 22:48:34 +0530

    feat(backend): :zap: updated User model

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,4 +1,4 @@
-use crate::{models::user::CreateUser, utils::state::AppState};
+use crate::{models::user::User, utils::state::AppState};
 use axum::{
     extract::{Path, State},
     http::StatusCode,
@@ -50,9 +50,9 @@ pub async fn get_user_by_id(
 
 pub async fn create_user(
     State(state): State<AppState>,
-    Json(payload): Json<CreateUser>,
+    Json(payload): Json<User>,
 ) -> impl IntoResponse {
-    let body = json!({"name": payload.name, "username": payload.username });
+    let body = json!({"name": payload.name, "username": payload.username, "email": payload.email, "dob": payload.dob, "password": payload.password, "auth_provider":payload.auth_provider});
     let response = state
         .supabase
         .from("Users")
@@ -63,7 +63,31 @@ pub async fn create_user(
         Ok(resp) => {
             let body = resp.text().await.unwrap();
             let json_body: Value = from_str(&body).unwrap();
-            (StatusCode::OK, Json(json!({"data": json_body}))).into_response()
+
+            let is_profile_complete = json_body
+                .get("is_profile_complete")
+                .and_then(|v| v.as_bool())
+                .unwrap_or(false);
+
+            if !is_profile_complete {
+                return (
+                    StatusCode::OK,
+                    Json(json!({
+                        "needs_profile_completion": true,
+                        "data": json_body
+                    })),
+                )
+                    .into_response();
+            }
+
+            (
+                StatusCode::OK,
+                Json(json!({
+                    "needs_profile_completion": false,
+                    "data": json_body
+                })),
+            )
+                .into_response()
         }
         Err(_) => (
             StatusCode::INTERNAL_SERVER_ERROR,
--- src/models/user.rs
+++ src/models/user.rs
@@ -1,7 +1,18 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-pub struct CreateUser {
+pub struct User {
     pub name: String,
-    pub username: String,
+
+    //option for none serde
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub username: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub email: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub dob: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub password: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub auth_provider: Option<String>,
 }
