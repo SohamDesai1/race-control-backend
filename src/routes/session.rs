69fbd6eb73c9fbbe30901d5bc2758da8ee245d61
SohamDesai1 <sohamdesai123@gmail.com>
2025-12-07 00:00:25 +0530

    feat(backend): :sparkles: added dashmap for driver positions

--- src/routes/session.rs
+++ src/routes/session.rs
@@ -1,7 +1,7 @@
 use crate::{
     handlers::{
         middleware::auth_middleware,
-        session::{get_session_data, fetch_telemetry, get_sessions, get_graph_data},
+        session::{get_session_data, fetch_driver_telemetry, get_sessions, get_drivers_position_telemetry},
     },
     utils::state::AppState,
 };
@@ -12,8 +12,8 @@ pub fn session_routes(state: Arc<AppState>) -> Router<Arc<AppState>> {
     let session_router = Router::new()
         .route("/get_sessions/{race_id}", get(get_sessions))
         .route("/get_session_data/{session_key}", get(get_session_data))
-        .route("/fetch_telemetry", get(fetch_telemetry))
-        .route("/get_graph_data/{session_key}", get(get_graph_data))
+        .route("/fetch_driver_telemetry", get(fetch_driver_telemetry))
+        .route("/get_drivers_position_telemetry/{session_key}", get(get_drivers_position_telemetry))
         .with_state(state.clone());
 
     session_router.layer(from_fn(move |req, next| {
